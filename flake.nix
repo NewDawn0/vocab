1d97c4f205df624558942a25a2174aa72fca6e56
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-06-08 07:33:03 +0200

    add tl

--- flake.nix
+++ flake.nix
@@ -3,16 +3,29 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     nix-systems.url = "github:nix-systems/default";
+    tl = {
+      url = "github:NewDawn0/tl";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nix-systems.follows = "nix-systems";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
     let eachSystem = nixpkgs.lib.genAttrs (import inputs.nix-systems);
     in {
       packages = eachSystem (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config = { };
+            overlays = [
+              (final: prev: { tl = inputs.tl.packages.${prev.system}.default; })
+            ];
+          };
         in {
           default = pkgs.python3Packages.buildPythonPackage {
             name = "vocab";
+            propagatedBuildInputs = with pkgs; [ tl ];
             src = ./.;
             buildInputs = with pkgs.python3Packages; [ setuptools ];
           };
